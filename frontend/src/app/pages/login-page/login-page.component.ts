909b75e3d46ac1dcdc2e48d5eb93ab0aaae848ab
Frank B <frankfonda25.fb@gmail.com>
2023-02-02 15:23:09 +0100

    Sign Up Complete

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -8,7 +9,7 @@ import { AuthService } from 'src/app/auth.service';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-  constructor (private authService: AuthService) {}
+  constructor (private authService: AuthService, private router: Router) {}
 
   ngOnInit(){
     
@@ -16,6 +17,10 @@ export class LoginPageComponent implements OnInit {
 
   onLoginButtonClicked(email: string, password: string) {
     this.authService.login(email, password).subscribe((res: HttpResponse<any>) => {
+      if (res.status === 200) {
+        this.router.navigate(['/lists']);
+
+      }
       console.log(res);
     })
   }
